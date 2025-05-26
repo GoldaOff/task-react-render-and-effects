ecbc7549d8ed737fd37e127dba8465417effc972
Andrey Ratushin <ratushin.andrey@mail.ru>
2025-05-26 16:09:25 +0300

    Задание

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const { sourceId } = props;
+
+    const [lastMessage, setLastMessage] = useState<number>(-1);
+
+    useEffect(() => {
+        const handler = (message: number) => {
+            setLastMessage(message);
+        };
+
+        subscribe(sourceId, handler);
+
+        return () => {
+            unsubscribe(sourceId, handler);
+            setLastMessage(-1);
+        };
+    }, [sourceId]);
+
+    return (
+        <div>
+            {sourceId}: {lastMessage}
+        </div>
+    );
 }
